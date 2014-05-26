54b9a76b88d109cd6b74cafd08dd4ac79a3326ac
yamamori <yamamori@sa.cs.titech.ac.jp>
2014-05-26 19:11:06 +0900

    Added the function of setting a image from the gallery of android into the imageView.
    Task No.7 done.

--- Yamamori-Kadai/src/com/example/yamamori_kadai/PlaceholderFragment3.java
+++ Yamamori-Kadai/src/com/example/yamamori_kadai/PlaceholderFragment3.java
@@ -1,7 +1,15 @@
 package com.example.yamamori_kadai;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +21,8 @@ import android.widget.Toast;
 
 public class PlaceholderFragment3 extends Fragment {
 	final private static int REQUEST_CODE = 0x655;
+	private ImageView image;
+	private View rootView;
 
 	public PlaceholderFragment3() {
 	}
@@ -20,9 +30,9 @@ public class PlaceholderFragment3 extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.image_fragment, container,
+		rootView = inflater.inflate(R.layout.image_fragment, container,
 				false);
-		ImageView image = (ImageView)rootView.findViewById(R.id.image);
+		image = (ImageView)rootView.findViewById(R.id.image);
 		image.setOnClickListener(new OnClickListener(){
 
 			@Override
@@ -40,11 +50,20 @@ public class PlaceholderFragment3 extends Fragment {
 	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data){
-		switch(requestCode){
-            case REQUEST_CODE:
-            	
-            	break;
-            default:
-		}
+        Context context = rootView.getContext();
+        if(requestCode == REQUEST_CODE&&resultCode==Activity.RESULT_OK){
+            try {
+                if(context!=null&&image!=null){
+                    InputStream in = context.getContentResolver().openInputStream(data.getData());
+                    Bitmap img=BitmapFactory.decodeStream(in);
+                    in.close();
+                    image.setImageBitmap(img);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 	}
 }
